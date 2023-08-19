d3ceed053ee058e4c640f6888239b347608100e7
GUSTAVO MARIN GOULART <gustavomaringoulart@gmail.com>
2023-08-19 15:41:42 -0300

    next

--- main.c
+++ main.c
@@ -1,26 +1,80 @@
 #include <stdio.h>
+#include <iostream>
+typedef enum
+{
+    // palavras reservadas
+    ARRAY,
+    BOOLEAN,
+    BREAK,
+    CHAR,
+    CONTINUE,
+    DO,
+    ELSE,
+    FALSE,
+    FUNCTION,
+    IF,
+    INTEGER,
+    OF,
+    STRING,
+    STRUCT,
+    TRUE,
+    TYPE,
+    VAR,
+    WHILE,
+    // simbolos
+    COLON,
+    SEMI_COLON,
+    COMMA,
+    EQUALS,
+    LEFT_SQUARE,
+    RIGHT_SQUARE,
+    LEFT_BRACES,
+    RIGHT_BRACES,
+    LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS,
+    AND,
+    OR,
+    LESS_THAN,
+    GREATER_THAN,
+    LESS_OR_EQUAL,
+    GREATER_OR_EQUAL,
+    NOT_EQUAL,
+    EQUAL_EQUAL,
+    PLUS,
+    PLUS_PLUS,
+    MINUS,
+    MINUS_MINUS,
+    TIMES,
+    DIVIDE,
+    DOT,
+    NOT,
+    // tokens regulares
+    CHARACTER,
+    NUMERAL,
+    STRINGVAL,
+    ID,
+    // token deconhecido
+    UNKNOWN
+} t_token;
 
-
-typedef enum {
-// palavras reservadas
-ARRAY, BOOLEAN, BREAK, CHAR, CONTINUE, DO, ELSE, FALSE, FUNCTION, IF,
-INTEGER, OF, STRING, STRUCT, TRUE, TYPE, VAR, WHILE,
-// simbolos
-COLON, SEMI_COLON, COMMA, EQUALS, LEFT_SQUARE, RIGHT_SQUARE,
-LEFT_BRACES, RIGHT_BRACES, LEFT_PARENTHESIS, RIGHT_PARENTHESIS, AND,
-OR, LESS_THAN, GREATER_THAN, LESS_OR_EQUAL, GREATER_OR_EQUAL,
-NOT_EQUAL, EQUAL_EQUAL, PLUS, PLUS_PLUS, MINUS, MINUS_MINUS, TIMES,
-DIVIDE, DOT, NOT,
-// tokens regulares
-CHARACTER, NUMERAL, STRINGVAL, ID,
-// token deconhecido
-UNKNOWN } t_token; 
-
+t_token searchKeyWord(char *name);
 char readChar(void);
 t_token nextToken(void);
 t_token token;
 int tokenSecundario;
 char nextChar = '\x20';
+int searchName(char *name);
+
+typedef struct
+{
+    byte type; // 0-char, 1-int, 2- string
+    union
+    {
+        char cVal;
+        int nVal;
+        char *sVal;
+    } _;
+} t_const;
 
 t_token nextToken(void)
 {
